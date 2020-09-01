a467cbe12da00c253e339e5d0ebcccf1593dd66b
William <mhf.eng.dsg@gmail.com>
2020-09-01 14:37:38 -0300

    ✨ Finish HeaderMain component with children

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HeaderMain from './components/HeaderMain';
+import LogoMain from './components/LogoMain';
+import IconSearch from './components/IconSearch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Busca Negócios React</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HeaderMain>
+        <LogoMain />
+        <IconSearch />
+      </HeaderMain>
+    </>
   );
 }
 
