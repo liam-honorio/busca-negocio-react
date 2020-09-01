8f2b02ffdf0a4031e6cd2bdaff6ddc6bffea34b8
William <mhf.eng.dsg@gmail.com>
2020-09-01 14:34:39 -0300

    🚧 Create LogoMain

---
+++ src/components/LogoMain/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import logo from '../../assets/images/logo.svg';
+
+const LogoMain = styled.img.attrs({ src: logo, alt: 'Logo da CollabCode' })`
+  width: 400px;
+`;
+
+export default LogoMain;
