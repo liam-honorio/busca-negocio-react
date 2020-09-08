875e5a0837bff3e6e496992e58d5d4947e1e4322
William <mhf.eng.dsg@gmail.com>
2020-09-08 10:51:05 -0300

    💄 Update style of ServiceFilter

--- src/components/ServiceFilter/styles.js
+++ src/components/ServiceFilter/styles.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
+import IconFilter from '../IconFilter';
 
 export const ServiceFilterStyle = styled.nav`
   background-color: #3a4042;
+  display: flex;
+
+  & > ${IconFilter} {
+    margin-right: 16px;
+  }
 `;
 
 export const Label = styled.p`
