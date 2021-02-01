6104f2d1dbd3730e5b07747fa76b3fb31a9ea64d
William <mhf.eng.dsg@gmail.com>
2021-02-01 17:33:32 -0300

    ✨ Add LogoMain component in storybook

---
+++ src/stories/components/LogoMain.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import LogoMain from '../../components/LogoMain';
+
+export default {
+    title: 'Components/LogoMain',
+    component: LogoMain,
+};
+
+export const Default = () => <LogoMain />;
