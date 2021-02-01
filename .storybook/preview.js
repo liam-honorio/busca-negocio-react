a5fd0af4d3532f82ae902a31e0f7a3944750e30c
William <mhf.eng.dsg@gmail.com>
2021-02-01 17:40:54 -0300

    ✨Change storybook's background

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: '#4c5b84',
+      },
+    ],
+  },
 }
