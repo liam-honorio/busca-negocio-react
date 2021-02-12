cf5bba5d8208e8926bfe4d88fe8e33514c30a30c
William <mhf.eng.dsg@gmail.com>
2021-02-12 20:04:25 -0300

    ✨Add IconSearch in storybook

---
+++ src/stories/components/IconSearch.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import IconSearch from '../../components/IconSearch';
+
+export default {
+    title: 'Components/IconSearch',
+    components: IconSearch,
+};
+
+export const Default = () => <IconSearch />;
