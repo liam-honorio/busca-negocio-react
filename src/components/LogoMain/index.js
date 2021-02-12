410d3fc6ef1669d24cef82d9a722e6dd9a9ee379
William <mhf.eng.dsg@gmail.com>
2021-02-12 20:00:33 -0300

    ♻️ Change width from LogoMain

--- src/components/LogoMain/index.js
+++ src/components/LogoMain/index.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import logo from '../../assets/images/logo.svg';
 
-const LogoMain = styled.img.attrs({ src: logo, alt: 'Logo da CollabCode' })`
-  width: 400px;
-`;
+const LogoMain = styled.img.attrs({ src: logo, alt: 'Logo da CollabCode' })``;
 
 export default LogoMain;
